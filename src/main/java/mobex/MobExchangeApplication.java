5456646dabd97a5905a2e49c9e54da3bd6bfb650
Sebi Barta <sebibarta11@gmail.com>
2024-07-09 13:38:29 +0300

    Initial commit

---
+++ src/main/java/mobex/MobExchangeApplication.java
@@ -0,0 +1,13 @@
+package mobex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobExchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobExchangeApplication.class, args);
+	}
+
+}
