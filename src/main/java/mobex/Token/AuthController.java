69d2d5160c7ee4ef0eae0b4569a787c0945731ae
Eugenne07 <sabaueugen02@gmail.com>
2024-07-16 13:20:35 +0300

    Update SecurityConfig and Services

--- src/main/java/mobex/Token/AuthController.java
+++ src/main/java/mobex/Token/AuthController.java
@@ -7,72 +7,60 @@ import mobex.User.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
 @RestController
+@RequestMapping("/auth")
 public class AuthController {
 
     private final UserService userService;
     private final AuthService authService;
-    private final UserRepository userRepository;
     private final TokenRepository tokenRepository;
 
     @Autowired
-    public AuthController(UserService userService, AuthService authService,
-                          UserRepository userRepository, TokenRepository tokenRepository) {
+    public AuthController(UserService userService, AuthService authService, TokenRepository tokenRepository) {
         this.userService = userService;
         this.authService = authService;
-        this.userRepository = userRepository;
         this.tokenRepository = tokenRepository;
     }
-
 
     @PostMapping("/register")
     public ResponseEntity<?> register(@RequestBody User userData){
         try{
             User user = userService.registerUser(userData);
             TokenDTO tokenDTO = authService.createTokenReturnDTO(user);
-
             return new ResponseEntity<>(tokenDTO, HttpStatus.CREATED);
         }
         catch (Exception e){
-            return new ResponseEntity<>("Email is already in use",HttpStatus.UNAUTHORIZED);
+            return new ResponseEntity<>("Email is already in use", HttpStatus.UNAUTHORIZED);
         }
     }
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody UserDTO userDTO){
         try{
-            Optional<User> userOptional = userRepository.findByEmail(userDTO.getEmail()); //find user
-            if(userOptional.isPresent()) {  //if exists
-
-                User user = userOptional.get(); // gets user info
-
-                if(user.getPassword().equals(userDTO.getPassword())){ //verifies pw
-
-                    TokenDTO tokenDTO = authService.createTokenReturnDTO(user);    //makes tokenDTO
-                    return new ResponseEntity<>(tokenDTO, HttpStatus.CREATED);
-                }}
-            return new ResponseEntity<>("Invalid credentials",HttpStatus.UNAUTHORIZED);
+            User user = userService.loginUser(userDTO);
+            TokenDTO tokenDTO = authService.createTokenReturnDTO(user);
+            return new ResponseEntity<>(tokenDTO, HttpStatus.CREATED);
         }
         catch (Exception e){
-            return new ResponseEntity<>("Invalid credentials",HttpStatus.UNAUTHORIZED);
+            return new ResponseEntity<>("Invalid credentials", HttpStatus.UNAUTHORIZED);
         }
     }
 
     @PostMapping("/logout")
     public ResponseEntity<?> logout(@RequestHeader("Authorization") String accessToken){
         try{
-             accessToken = accessToken.substring(7);
+            accessToken = accessToken.substring(7); // Remove "Bearer " prefix
             Optional<Token> tokenOptional = tokenRepository.findTokenByAccessToken(accessToken);
             tokenOptional.ifPresent(tokenRepository::delete);
-            return new ResponseEntity<>("Token deleted",HttpStatus.OK);
+            return new ResponseEntity<>("Token deleted", HttpStatus.OK);
         }
         catch(Exception e){
-            return new ResponseEntity<>("scsdcsd",HttpStatus.UNAUTHORIZED);
+            return new ResponseEntity<>("Invalid token", HttpStatus.UNAUTHORIZED);
         }
     }
 }
-
--- src/main/java/mobex/User/UserService.java
+++ src/main/java/mobex/User/UserService.java
@@ -2,6 +2,7 @@ package mobex.User;
 
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -10,24 +11,33 @@ import java.util.Optional;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Transactional
     public User registerUser(User user){
-        userRepository.save(user);
-        return user;
+        try{
+            String hashedPassword = passwordEncoder.encode(user.getPassword());
+            user.setPassword(hashedPassword);
+            userRepository.save(user);
+            return user;
+        }
+        catch(Error e){
+            throw new RuntimeException(e.getMessage());
+        }
     }
 
-    public UserDTO loginUser(UserDTO userDTO){
+    public User loginUser(UserDTO userDTO){
         Optional<User> userOptional = userRepository.findByEmail(userDTO.getEmail());
         if(userOptional.isPresent()){
             User user = userOptional.get();
-            if(user.getPassword().equals(userDTO.getPassword())){
-                return userDTO;
+            if(passwordEncoder.matches(userDTO.getPassword(), user.getPassword())){
+                return user;
             }
             else {
                 throw new RuntimeException("Invalid Password");
