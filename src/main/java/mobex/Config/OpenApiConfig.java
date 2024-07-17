c675f7e39cffbab7c3bfc1e57696ffa6ef1fb759
Geo <popovicigeorge3@gmail.com>
2024-07-17 13:38:08 +0300

    Fully Implemented Endpoints with Swagger

--- src/main/java/mobex/Config/OpenApiConfig.java
+++ src/main/java/mobex/Config/OpenApiConfig.java
@@ -1,18 +1,15 @@
 package mobex.Config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
 
 @OpenAPIDefinition(
             info = @Info(
-                    title = "Exchange Rate API",
+                    title = "MobExchange API",
                     version = "1.0",
-                    description = "API for getting exchange rates",
+                    description = "API for getting exchange rates and managing user authentication.",
                     license = @License(
                             name = "Apache 2.0",
                             url = "http://www.apache.org/licenses/LICENSE-2.0.html"
@@ -22,14 +19,7 @@ import io.swagger.v3.oas.annotations.servers.Server;
             {
                 @Server(url = "http://localhost:8080", description = "Local server"),
             }
-)
-@SecurityScheme(
-        name = "BasicAuth",
-        description = "JWT auth description",
-        scheme = "bearer",
-        type = SecuritySchemeType.HTTP,
-        bearerFormat = "JWT",
-        in = SecuritySchemeIn.HEADER
+
 )
 public class OpenApiConfig {
 }
--- src/main/java/mobex/ExchangeRate/ExchangeRateController.java
+++ src/main/java/mobex/ExchangeRate/ExchangeRateController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/exchange-rates")
-@Tag(name = "Exchange Rate APIs")
+@Tag(name = "Exchange Rate Management", description = "Endpoints for retrieving and converting exchange rates.")
 public class ExchangeRateController {
 
     private final ExchangeRateService exchangeRateService;
@@ -19,13 +19,13 @@ public class ExchangeRateController {
 
     // Get latest exchange rates for a defined currency
     @Operation(summary = "Get latest exchange rates for a defined currency",
-    responses = {
-            @ApiResponse(description = "Exchange rates retrieved successfully",
-                    responseCode = "200"),
-            @ApiResponse(
-                    description = "Unauthorized / Invalid Token",
-                    responseCode = "403")
-    })
+            responses = {
+                    @ApiResponse(description = "Exchange rates retrieved successfully",
+                            responseCode = "200"),
+                    @ApiResponse(
+                            description = "Unauthorized / Invalid Token",
+                            responseCode = "403")
+            })
     @GetMapping("/latest/{baseCurrency}")
     public String getLatestRates(
             @Parameter(description = "The base currency for which to get the latest exchange rates")
@@ -34,18 +34,32 @@ public class ExchangeRateController {
     }
 
     // Get exchange rates for a specific date and currency
-    @Operation(summary = "Get exchange rates for a specific date and currency")
+    @Operation(summary = "Get exchange rates for a specific date and currency",
+            responses = {
+                    @ApiResponse(description = "Exchange rates retrieved successfully",
+                            responseCode = "200"),
+                    @ApiResponse(
+                            description = "Unauthorized / Invalid Token",
+                            responseCode = "403")
+            })
     @GetMapping("/{date}/{baseCurrency}")
     public String getExchangeRatesForDate(
             @Parameter(description = "The date for which to get the exchange rates in YYYY-MM-DD format")
             @PathVariable String date,
             @Parameter(description = "The base currency for which to get the exchange rates")
-            @PathVariable String baseCurrency){
+            @PathVariable String baseCurrency) {
         return exchangeRateService.getRatesForDate(date, baseCurrency);
     }
 
     // All rates for a specific period for a defined currency
-    @Operation(summary = "Get all rates for a specific period for a defined currency")
+    @Operation(summary = "Get all rates for a specific period for a defined currency",
+            responses = {
+                    @ApiResponse(description = "Exchange rates retrieved successfully",
+                            responseCode = "200"),
+                    @ApiResponse(
+                            description = "Unauthorized / Invalid Token",
+                            responseCode = "403")
+            })
     @GetMapping("/period/{startDate}/{endDate}/{baseCurrency}")
     public String getExchangeRatesForPeriod(
             @Parameter(description = "The start date of the period in YYYY-MM-DD format")
@@ -58,7 +72,14 @@ public class ExchangeRateController {
     }
 
     // Historical exchange rates between two currencies for a defined period
-    @Operation(summary = "Get historical exchange rates between two currencies for a defined period")
+    @Operation(summary = "Get historical exchange rates between two currencies for a defined period",
+            responses = {
+                    @ApiResponse(description = "Exchange rates retrieved successfully",
+                            responseCode = "200"),
+                    @ApiResponse(
+                            description = "Unauthorized / Invalid Token",
+                            responseCode = "403")
+            })
     @GetMapping("/historical/{startDate}/{endDate}/{baseCurrency}/{targetCurrency}")
     public String getHistoricalRatesBetweenCurrencies(
             @Parameter(description = "The start date of the period in YYYY-MM-DD format")
@@ -73,7 +94,14 @@ public class ExchangeRateController {
     }
 
     // Endpoint to get historical rates for two currencies from a specific date until today
-    @Operation(summary = "Get historical rates for two currencies from a specific date until today")
+    @Operation(summary = "Get historical rates for two currencies from a specific date until today",
+            responses = {
+                    @ApiResponse(description = "Exchange rates retrieved successfully",
+                            responseCode = "200"),
+                    @ApiResponse(
+                            description = "Unauthorized / Invalid Token",
+                            responseCode = "403")
+            })
     @GetMapping("/historical/{startDate}/{baseCurrency}/{targetCurrency}")
     public String getHistoricalRatesBetweenCurrenciesFromToNow(
             @Parameter(description = "The start date from which to get the exchange rates in YYYY-MM-DD format")
@@ -86,7 +114,14 @@ public class ExchangeRateController {
     }
 
     // Endpoint to convert currency from one to another using the latest rates
-    @Operation(summary = "Convert currency from one to another using the latest rates")
+    @Operation(summary = "Convert currency from one to another using the latest rates",
+            responses = {
+                    @ApiResponse(description = "Exchange rates retrieved successfully",
+                            responseCode = "200"),
+                    @ApiResponse(
+                            description = "Unauthorized / Invalid Token",
+                            responseCode = "403")
+            })
     @GetMapping("/convert")
     public Double convertCurrency(
             @Parameter(description = "The amount of currency to convert")
